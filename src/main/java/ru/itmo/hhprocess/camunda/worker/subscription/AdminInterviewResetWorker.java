22c2293c561c250b3a5a22269229164423b46199
msuny-c <munirbairamgulov@gmail.com>
2026-06-16 00:27:10 +0300

    update

--- src/main/java/ru/itmo/hhprocess/camunda/worker/subscription/AdminInterviewResetWorker.java
+++ src/main/java/ru/itmo/hhprocess/camunda/worker/subscription/AdminInterviewResetWorker.java
@@ -7,17 +7,37 @@ import ru.itmo.hhprocess.utils.CamundaTaskVariables;
 import ru.itmo.hhprocess.service.InterviewProcessService;
 
 import java.util.Map;
+import java.util.UUID;
 
 @Component
 @ConditionalOnProperty(prefix = "app.camunda.worker", name = "enabled", havingValue = "true", matchIfMissing = true)
 @CamundaWorkerSubscriptions.AdminInterviewReset
-public class AdminInterviewResetWorker extends InterviewFlowWorker {
-    public AdminInterviewResetWorker(CamundaFormValidator formValidator, InterviewProcessService interviewProcessService) {
-        super(formValidator, interviewProcessService);
+public class AdminInterviewResetWorker extends AbstractExternalTaskWorker {
+
+    private final InterviewProcessService interviewProcessService;
+
+    public AdminInterviewResetWorker(CamundaFormValidator formValidator,
+                                     InterviewProcessService interviewProcessService) {
+        super(formValidator);
+        this.interviewProcessService = interviewProcessService;
     }
 
     @Override
     protected Map<String, Object> handle(String activityId, CamundaTaskVariables variables) {
-        return handleAdminReset(activityId, variables);
+        UUID interviewId = variables.readRequiredUuid("interviewId");
+        UUID adminUserId = variables.readRequiredUuid("adminUserId");
+        String resetReason = variables.stringValue("resetReason");
+        return switch (activityId) {
+            case "ValidateAdminResetForm" -> interviewProcessService.validateAdminResetForm(
+                    interviewId, adminUserId, resetReason);
+            case "ValidateInterviewCanBeReset" -> interviewProcessService.validateInterviewCanBeReset(
+                    interviewId, adminUserId);
+            case "CancelInterviewByAdmin" -> interviewProcessService.cancelInterviewByAdmin(interviewId, resetReason);
+            case "ReleaseAdminResetSlot" -> interviewProcessService.releaseAdminResetSlot(interviewId);
+            case "ReturnApplicationToReview" -> interviewProcessService.returnApplicationToReview(
+                    interviewId, adminUserId, resetReason);
+            case "RecordAdminResetHistory" -> interviewProcessService.recordAdminResetHistory(interviewId, adminUserId);
+            default -> Map.of("adminInterviewResetIgnored", true, "activityId", activityId);
+        };
     }
 }
--- src/main/java/ru/itmo/hhprocess/camunda/worker/subscription/InterviewCancelWorker.java
+++ src/main/java/ru/itmo/hhprocess/camunda/worker/subscription/InterviewCancelWorker.java
@@ -7,17 +7,37 @@ import ru.itmo.hhprocess.utils.CamundaTaskVariables;
 import ru.itmo.hhprocess.service.InterviewProcessService;
 
 import java.util.Map;
+import java.util.UUID;
 
 @Component
 @ConditionalOnProperty(prefix = "app.camunda.worker", name = "enabled", havingValue = "true", matchIfMissing = true)
 @CamundaWorkerSubscriptions.InterviewCancel
-public class InterviewCancelWorker extends InterviewFlowWorker {
-    public InterviewCancelWorker(CamundaFormValidator formValidator, InterviewProcessService interviewProcessService) {
-        super(formValidator, interviewProcessService);
+public class InterviewCancelWorker extends AbstractExternalTaskWorker {
+
+    private final InterviewProcessService interviewProcessService;
+
+    public InterviewCancelWorker(CamundaFormValidator formValidator,
+                                 InterviewProcessService interviewProcessService) {
+        super(formValidator);
+        this.interviewProcessService = interviewProcessService;
     }
 
     @Override
     protected Map<String, Object> handle(String activityId, CamundaTaskVariables variables) {
-        return handleCancel(activityId, variables);
+        UUID interviewId = variables.readRequiredUuid("interviewId");
+        UUID recruiterUserId = variables.readUuid("recruiterUserId");
+        String cancelReason = variables.stringValue("cancelReason");
+        return switch (activityId) {
+            case "ValidateRecruiterCancelInterview" -> interviewProcessService.validateRecruiterCancelInterview(
+                    interviewId, recruiterUserId, variables.stringValue("starterUserId"), cancelReason);
+            case "CancelInterviewByRecruiter" -> interviewProcessService.cancelInterviewByRecruiter(
+                    interviewId, recruiterUserId, variables.stringValue("starterUserId"), cancelReason);
+            case "ReleaseRecruiterCancelSlot" -> interviewProcessService.releaseRecruiterCancelSlot(interviewId);
+            case "ReturnCancelApplicationToReview" -> interviewProcessService.returnCancelApplicationToReview(
+                    interviewId, recruiterUserId, variables.stringValue("starterUserId"), cancelReason);
+            case "RecordRecruiterCancelHistory" -> interviewProcessService.recordRecruiterCancelHistory(
+                    interviewId, recruiterUserId, variables.stringValue("starterUserId"));
+            default -> Map.of("interviewCancelIgnored", true, "activityId", activityId);
+        };
     }
 }
